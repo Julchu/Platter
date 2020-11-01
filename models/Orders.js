d06603e77229131aa5dc22e7d8bd5941e4242f00
Julian <jchumtl@gmail.com>
2020-10-31 20:47:14 -0400

    Spacing

--- models/Orders.js
+++ models/Orders.js
@@ -3,10 +3,10 @@
 let mongoose = require("mongoose");
 
 var orderSchema = new mongoose.Schema({
-    startTime: {type: Date, required: true},
-    endTime: {type: Date, required: true},
-    caterer: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true},
-    customer: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true}
+	startTime: {type: Date, required: true},
+	endTime: {type: Date, required: true},
+	caterer: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true},
+	customer: {type: mongoose.Schema.Types.ObjectId, ref: 'Users', required: true}
 });
 
 let Order = mongoose.model("Order", orderSchema, "Orders");
